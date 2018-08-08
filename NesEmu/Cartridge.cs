1a5b63c2e5c821b36cc43e05a54d253afa70145a
permil <mame9696@hotmail.com>
2018-08-09 01:41:13 +0900

    Add iNES loader

---
+++ NesEmu/Cartridge.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace NesEmu
+{
+    class Cartridge
+    {
+        public byte[] PRG;
+        public byte[] CHR; // includes the case of CHRRAM
+        private bool UsesCHRRAM { get; set; }
+
+        // https://wiki.nesdev.com/w/index.php/INES#iNES_file_format
+
+        // 76543210
+        // ||||||||
+        // |||||||+-Mirroring: 0: horizontal(vertical arrangement)(CIRAM A10 = PPU A11)
+        // |||||||             1: vertical(horizontal arrangement)(CIRAM A10 = PPU A10)
+        // ||||||+--1: Cartridge contains battery - backed PRG RAM ($6000 - 7FFF) or other persistent memory
+        // |||||+---1: 512 - byte trainer at $7000 -$71FF(stored before PRG data)
+        // ||||+----1: Ignore mirroring control or above mirroring bit; instead provide four - screen VRAM
+        // ++++---- - Lower nybble of mapper number
+        private byte flag6;
+        public bool VerticalMirroring { get { return (flag6 & 0x01) != 0; } }
+        public bool HasBattery        { get { return (flag6 & 0x02) != 0; } }
+        private bool ContainsTrainer   { get { return (flag6 & 0x04) != 0; } }
+
+        // 76543210
+        // ||||||||
+        // |||||||+-VS Unisystem
+        // ||||||+--PlayChoice - 10(8KB of Hint Screen data stored after CHR data)
+        // ||||++---If equal to 2, flags 8 - 15 are in NES 2.0 format
+        // ++++---- - Upper nybble of mapper number
+        private byte flag7;
+        public int Mapper { get { return flag7 & 0xF0 | (flag6 >> 4 & 0xF); } }
+
+        public static async Task<Cartridge> LoadFromNES(StorageFile file)
+        {
+            Cartridge cartridge;
+            using (Stream stream = (await file.OpenReadAsync()).AsStreamForRead())
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    cartridge = Create(reader);
+                }
+            }
+
+            return cartridge;
+        }
+
+        private Cartridge() { }
+        private static Cartridge Create(BinaryReader reader)
+        {
+            if (reader.ReadUInt32() != 0x1A53454E)
+            {
+                return null; // validation error
+            }
+
+            Cartridge cartridge = new Cartridge();
+
+            // read header
+            int PRGROMSize = reader.ReadByte(); // 16KB * size
+            int CHRROMSize = reader.ReadByte(); // 8KB * size, 0 means CHR RAM (unimplemented)
+            cartridge.UsesCHRRAM = (CHRROMSize == 0);
+            cartridge.flag6 = reader.ReadByte();
+            cartridge.flag7 = reader.ReadByte();
+            int PRGRAMSize = Math.Min((int)reader.ReadByte(), 1); // 8KB * size, 0 infers 8 KB for compatibility
+            byte flag9 = reader.ReadByte();
+            byte flag10 = reader.ReadByte();
+            reader.ReadBytes(5); // 11-15: Zero filled
+
+            // read trainer (ignore)
+            if (cartridge.ContainsTrainer) { reader.ReadBytes(512); }
+
+            // read PRGROM
+            cartridge.PRG = new byte[PRGROMSize * 16384];
+            reader.Read(cartridge.PRG, 0, cartridge.PRG.Length);
+
+            // read CHRROM or init CHRRAM
+            if (cartridge.UsesCHRRAM)
+            {
+                cartridge.CHR = new byte[8192];
+            }
+            else
+            {
+                cartridge.CHR = new byte[CHRROMSize * 8192];
+                reader.Read(cartridge.CHR, 0, cartridge.CHR.Length);
+            }
+
+            return cartridge;
+        }
+    }
+}
--- NesEmu/MainPage.xaml.cs
+++ NesEmu/MainPage.xaml.cs
@@ -6,12 +6,15 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
@@ -25,6 +28,61 @@ namespace NesEmu
         public MainPage()
         {
             this.InitializeComponent();
+
+
+            // BitmapImageの準備
+            int width = 240;
+            int height = 240;
+            WriteableBitmap bitmap = new WriteableBitmap(width, height);
+            this.image.Source = bitmap;
+
+            // 計算用のバイト列の準備
+            int pixelsSize = (int)(width * height * 4);
+            byte[] pixels = new byte[pixelsSize];
+
+            // バイト列に色情報を入れる
+            byte value = 0;
+            for (int x = 0; x < width * height * 4; x = x + 4)
+            {
+                byte blue = value;
+                byte green = value;
+                byte red = value;
+                byte alpha = 255;
+                pixels[x] = blue;
+                pixels[x + 1] = green;
+                pixels[x + 2] = red;
+                pixels[x + 3] = alpha;
+                value = (byte)((value + 1) % 240);
+            }
+
+            // バイト列をBitmapImageに変換する
+            using (var pixelStream = bitmap.PixelBuffer.AsStream())
+            {
+                pixelStream.Seek(0, SeekOrigin.Begin);
+                pixelStream.Write(pixels, 0, pixels.Length);
+            }
+
+            // Redraw the WriteableBitmap
+            bitmap.Invalidate();
+
+            Start();
+        }
+
+        private async void Start()
+        {
+            Cartridge cartridge = await LoadNESFileAsync();
+            Debug.WriteLine(cartridge.PRG.Length);
+            Debug.WriteLine(cartridge.CHR.Length);
+            Debug.WriteLine(cartridge.Mapper);
+        }
+
+        private async Task<Cartridge> LoadNESFileAsync()
+        {
+            var filePicker = new Windows.Storage.Pickers.FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".nes");
+            var file = await filePicker.PickSingleFileAsync();
+
+            return await Cartridge.LoadFromNES(file);
         }
     }
 }
